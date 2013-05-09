158797b5e5be8a807a6387db863a0af65522040a
Mark Renz Tiratira <matiratira@philweb.com.ph>
2013-05-09 11:38:06 +0800

    VMS->Commit missed module
    Reimbursable Voucher Controller

--- VMS/protected/controllers/ReimbursablevoucherController.php
+++ VMS/protected/controllers/ReimbursablevoucherController.php
@@ -1,27 +1,67 @@
 <?php
 
-class ReimbursableVoucherController extends Controller
+class ReimbursablevoucherController extends VMSBaseIdentity
 {
+        public $showDialog = false;
+        public $dialogMsg;
+    
 	public function actionIndex()
 	{
+            AuditLog::logTransactions(25);
             $model = new ReimbursableVoucherForm();
+            Yii::app()->session['disable'] = true;
             if(isset($_POST['ReimbursableVoucherForm']))
             {
                 $model->attributes=$_POST['ReimbursableVoucherForm'];
                 $data=$model->attributes;
                 Yii::app()->session['rvfrom'] = $data['from']. ' 06:00:00';
                 Yii::app()->session['rvto'] = $data['to']. ' 06:00:00';
+                Yii::app()->session['site'] = $data['site'];
+                Yii::app()->session['terminal'] = $data['terminal'];
                 
                 $from = Yii::app()->session['rvfrom'];
                 $to = Yii::app()->session['rvto'];
-                
+                $site = Yii::app()->session['site'];
+                $terminal = Yii::app()->session['terminal'];
+                //print_r($_POST['ReimbursableVoucherForm']['from']);
                 if($model->validate())
                 {
-                    //echo 'test';
-                    $rawData = $model->getReimbursableVoucher($from, $to);
-                    Yii::app()->session['rawData'] = $rawData;
-                    $display = 'block';
-                    Yii::app()->session['display'] = $display;
+                    if(isset($_POST['yt0']))
+                    {
+                        $rawData = $model->getReimbursableVoucher($from, $to, $site, $terminal);
+                        Yii::app()->session['rawData'] = $rawData;
+                        $display = 'block';
+                        Yii::app()->session['display'] = $display;
+                        Yii::app()->session['disable'] = false;
+                    }
+                    else
+                    {
+                        if(isset($_POST['forreimburse']))
+                        {
+                            $forreimburse = $_POST['forreimburse'];
+                            $reimburselist = array();
+                            foreach($forreimburse as $value)
+                            {
+                                $reimburselist[] = "'".$value."'";
+                            }
+			    Yii::app()->session['reimburselist'] = $reimburselist;
+                            $vouchercode = implode(",",$reimburselist);
+                            Yii::app()->session['disable'] = false;
+
+                            //echo $r;
+                            $model->getReimburseVoucher($vouchercode);
+                            $rawData = $model->getReimbursableVoucher($from, $to, $site, $terminal);
+                            Yii::app()->session['rawData'] = $rawData;
+                            $this->showDialog = true;
+                            $this->dialogMsg = "Successfully Reimburse Voucher(s)!";
+                        }
+                        else
+                        {
+                            Yii::app()->session['disable'] = false;
+                            $this->showDialog = true;
+                            $this->dialogMsg = "No Vouchers selected!";
+                        }
+                    }
                 }
             }
             else
@@ -30,17 +70,21 @@ class ReimbursableVoucherController extends Controller
                 {
                     $from = Yii::app()->session['rvfrom'];
                     $to = Yii::app()->session['rvto'];
+                    $site = Yii::app()->session['site'];
+                    $terminal = Yii::app()->session['terminal'];
                     
-                    $rawData = $model->getReimbursableVoucher($from, $to);
+                    $rawData = $model->getReimbursableVoucher($from, $to, $site, $terminal);
                     Yii::app()->session['rawData'] = $rawData;
                     $display = 'block';
                     Yii::app()->session['display'] = $display;
+                    Yii::app()->session['disable'] = false;
                 }
                 else
                 {
                     Yii::app()->session['rawData'] = array(1);
                     $display = 'none';
                     Yii::app()->session['display'] = $display;
+                    Yii::app()->session['disable'] = true;
                 }
             }
             
@@ -50,7 +94,6 @@ class ReimbursableVoucherController extends Controller
         public function actionReimbursableVoucherDataTable($rawData)
         {
             $arrayDataProvider = new CArrayDataProvider($rawData, array(
-		'keyField'=>'id',
                 /*'id'=>'reimbursablevoucher-grid',
                 'sort'=>array(
                     'attributes'=>array('DateCreated','DateExpiry','Status'),
@@ -80,6 +123,29 @@ class ReimbursableVoucherController extends Controller
             $model = new ReimbursableVoucherForm();
             $terminal = $model->getTerminal($site);
             echo $terminal;
+        }
+        
+        public function actionExportToCSV()
+        {
+            AuditLog::logTransactions(26);
+            Yii::import('ext.ECSVExport');
+            $model = new ReimbursableVoucherForm();
+            
+            $rawData = Yii::app()->session['rawData'];
+            
+            //$currentdir = dirname(__FILE__) . '/';
+            //$rootdir = realpath($currentdir . '../') . '/';
+            $filename = "Reimbursable_Vouchers_".Date('Y_m_d').".csv";
+            
+            $csv = new ECSVExport($rawData);
+            
+            $csv->toCSV($filename);
+            
+            $content = file_get_contents($filename);
+            
+            Yii::app()->getRequest()->sendFile($filename, $content, "text/csv", false);
+            exit();
+            //unlink($filename.'csv');
         }
 
 	// Uncomment the following methods and override them if needed
