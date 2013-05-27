0ddd2a6469b32d9be0c662c9f7293c822662ebda
Oliver Candelario <ofcandelario@philweb.com.ph>
2013-05-27 20:49:43 +0800

    VMS > Fix pagination

--- VMS/yii/framework/web/CPagination.php
+++ VMS/yii/framework/web/CPagination.php
@@ -201,10 +201,10 @@ class CPagination extends CComponent
 	public function createPageUrl($controller,$page)
 	{
 		$params=$this->params===null ? $_GET : $this->params;
-		//if($page>0) // page 0 is the default
-			//$params[$this->pageVar]=$page+1;
-		//else
-			//unset($params[$this->pageVar]);
+		if($page>0) // page 0 is the default
+			$params[$this->pageVar]=$page+1;
+		else
+			unset($params[$this->pageVar]);
 		return $controller->createUrl($this->route,$params);
 	}
 
