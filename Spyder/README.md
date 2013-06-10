4996ab622b50bb4a6c7fbdcc4229b0e44ec18fd6
ITS Web Administrator <itswebadmin@gmail.com>
2013-06-10 14:42:41 +0800

    Version 1.0.0.2 June 10, 2013 ----------------------------- - remove MQ support - changed server admin interfacing
    File changes:
    Spyder\SAPI\amqp.php Deleted
    Spyder\SAPI\amqpwire.php Deleted
    Spyder\SAPI\hexdump.php Deleted
    Spyder\SAPI\index.php Modified

--- Spyder/README.md
+++ Spyder/README.md
@@ -1,10 +1,21 @@
 PeGS-3.0 - SAPI Web Service
 ===========================
 
-Latest Release: 1.0.0.1 (May 07, 2013)
+Latest Release: 1.0.0.2 (June 10, 2013)
 
-Status: Development Release Alpha 1
+Status: Development Release Alpha 2
 
 Version 1.0.0.1 May 07, 2013
 ----------------------------
 - initial development release alpha
+
+Version 1.0.0.2 June 10, 2013
+-----------------------------
+- remove MQ support
+- changed server admin interfacing
+
+File changes:
+Spyder\SAPI\amqp.php Deleted
+Spyder\SAPI\amqpwire.php Deleted
+Spyder\SAPI\hexdump.php Deleted
+Spyder\SAPI\index.php Modified
--- Spyder/SAPI/index.php
+++ Spyder/SAPI/index.php
@@ -1,29 +1,78 @@
 <?php
-
-header("Cache-Control: no-cache, must-revalidate");
-define('HOST', '192.168.30.135');
-define('PORT', '5672');
-define('USER', 'guest');
-define('PASS', 'guest');
-define('VHOST', '/');
-define('AMQP_DEBUG', true);
-require 'amqp.php';
+$dbhost="192.168.30.135";
+$dbusername="root";
+$dbpassword="password";
+$dbname="spyder";
+$spyderhost="127.0.0.1";
+$spyderport="35000";
 
 function getroutingkey($terminal) {
-
-    $con = mysqli_connect("192.168.28.149", "spyderapp", "password", "spyder");
-    if (mysqli_connect_errno($con)) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    $con = new mysqli($dbhost, $dbusername, $dbpassword, $dbname);
+    if ($con->connect_errno) {
+        echo "Failed to connect to MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
     }
-    $query = "select alias from connection where terminalname='$terminal'";
 
-    $result = mysqli_query($con, $query);
+//    $query = "SELECT a.ChannelID as channelid,b.ExtBindAddress as ip, b.ExtPort as port 
+//        FROM terminalconnections as a INNER JOIN servers as b ON a.ServerID = b.ServerID and a.TerminalID = ?";
 
-    $row = mysqli_fetch_array($result);
+    $query = "SELECT ipaddress FROM connection where terminalname=?";
+    
+    if (!($stmt = $con->prepare($query))) {
+        echo "Prepare failed: (" . $con->errno . ") " . $con->error;
+    }
 
-    mysqli_close($con);
+    if (!$stmt->bind_param("s", $terminal)) {
+        echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
 
-    return $row['alias'];
+    if (!$stmt->execute()) {
+        echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    }
+
+    $stmt->bind_result($district);
+
+    /* fetch value */
+    $stmt->fetch();
+    $stmt->close();
+    $con->close();
+    return $district;
+}
+
+function sendtospyder($str, $channel) {
+//Connect to Server
+    $command="/send?clientid={$channel}&msg={$str}\n\r";
+
+    $socket = stream_socket_client("tcp://{$spyderhost}:{$spyderport}", $errno, $errstr, 30);
+		
+    if ($socket) {
+
+        //Start SSL
+
+        stream_set_blocking($socket, true);
+
+        stream_socket_enable_crypto($socket, true, STREAM_CRYPTO_METHOD_SSLv3_CLIENT);
+
+        stream_set_blocking($socket, false);
+        //Send a command
+
+        fwrite($socket, $command);
+
+        $buf = null;
+
+        //Receive response from server. Loop until the response is finished
+//        while (!feof($socket)) {
+//
+//            $buf .= fread($socket, 20240);
+//        }
+        //close connection
+
+        fclose($socket);
+        //echo our command response
+
+        echo $buf;
+    } else {
+        echo $errno;
+    }
 }
 
 try {
@@ -33,48 +82,55 @@ try {
     $password = $_GET['Password'];
     $type = $_GET['Type'];
     $casinoid = $_GET['CasinoID'];
-    if (!isset($_GET['TerminalName']) || $terminalid==NULL) {
+    if (!isset($_GET['TerminalName']) || $terminalid == NULL) {
 //        echo "99";  
         echo "0";
-    } elseif (!isset($_GET['CommandID']) || $mode==NULL) {
+    } elseif (!isset($_GET['CommandID']) || $mode == NULL) {
 //        echo "98";
         echo "0";
-    } elseif (!isset($_GET['UserName']) || $username==NULL) {
+    } elseif (!isset($_GET['UserName']) || $username == NULL) {
 //        echo "97";
         echo "0";
-    } elseif (!isset($_GET['Password']) || $password==NULL) {
+    } elseif (!isset($_GET['Password']) || $password == NULL) {
 //        echo "96";
         echo "0";
-    } elseif (!isset($_GET['Type']) || $type==NULL) {
+    } elseif (!isset($_GET['Type']) || $type == NULL) {
 //        echo "95";
         echo "0";
-    } elseif (!isset($_GET['CasinoID']) || $casinoid==NULL) {
+    } elseif (!isset($_GET['CasinoID']) || $casinoid == NULL) {
 //        echo "94";
         echo "0";
     } else {
-        $connection = new AMQPConnection(HOST, PORT, USER, PASS, VHOST);
-        $channel = $connection->channel();
-
-        $channel->exchange_declare('sapi_ex', 'direct', false, false, false);
-
-//    $data = implode(' ', array_slice($argv, 1));
-//    if (empty($data))
-//        $data = "lock";
-
+//        list($channelid,$ip,$port)=getroutingkey($terminalid);
+        $ip=getroutingkey($terminalid);
         if ($mode == "1") {
-            $message = "lock|" . $username . "|" . $password . "|" . $casinoid;
-            $msg = new AMQPMessage($message);
-            $channel->basic_publish($msg, 'sapi_ex', getroutingkey($terminalid));
+//            $arrtype = array('Header' => 'Type', 'Data' => '0');
+//            $arrmessage = array('Header' => 'Message', 'Data' => 'lock');
+//            $arrusername = array('Header' => 'Username', 'Data' => $username);
+//            $arrpassword = array('Header' => 'Password', 'Data' => $password);
+//            $arrcasinoid = array('Header' => 'CasinoID', 'Data' => $casinoid);
+//            $payload = array($arrtype, $arrmessage, $arrusername, $arrpassword, $arrcasinoid);
+//            $arr = array('Command' => "lock", 'Version' => "1.0.0", 'PayLoad' => $payload, 'PayLoadCount' => count($payload));
+//            $message = json_encode($arr);
+//            sendtospyder($message,$ip,$port,$channelid);
+            $message="lock|" . $username . "|" . $password . "|" . $casinoid;
+            sendtospyder($message,$ip);
             echo "1";
         } elseif ($mode == "0") {
+//            $arrtype = array('Header' => 'Type', 'Data' => '1');
+//            $arrmessage = array('Header' => 'Message', 'Data' => 'unlock');
+//            $arrusername = array('Header' => 'Username', 'Data' => $username);
+//            $arrpassword = array('Header' => 'Password', 'Data' => $password);
+//            $arrcasinoid = array('Header' => 'CasinoID', 'Data' => $casinoid);
+//            $payload = array($arrtype, $arrmessage, $arrusername, $arrpassword, $arrcasinoid);
+//            $arr = array('Command' => "unlock", 'Version' => "1.0.0", 'PayLoad' => $payload, 'PayLoadCount' => count($payload));
+//            $message = json_encode($arr);
+//            sendtospyder($message,$ip,$port,$channelid);
             $message = "unlock|" . $username . "|" . $password . "|" . $casinoid;
-            $msg = new AMQPMessage($message);
-            $channel->basic_publish($msg, 'sapi_ex', getroutingkey($terminalid));
+            sendtospyder($message,$ip);
             echo "1";
         }else
             echo "0";
-        $channel->close();
-        $connection->close();
     }
 } catch (Exception $exc) {
     # $this->SMS->insertunsuccessfulsms($str);
