17b96d87c87296919b6ab2e46952fa411bb8a357
Edson L. Perez <elperez@philweb.com.ph>
2013-08-01 15:19:12 +0800

    commit reverse proxy update
    Membership_Release Notes_REF17_080113

--- Membership/templates/footertemplate.php
+++ Membership/templates/footertemplate.php
@@ -4,31 +4,29 @@
 
                 <div class="row-fluid">
                     <div class="span10 offset1 " id="footer-nav-wrapper">
-                        
-                        <div class="menu-footer-menu-container"><ul id="footer-navigation" class="menu"><li id="menu-item-28" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-28"><a href="http://www.egamescasino.ph/">Home</a></li>
-<li id="menu-item-30" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-30"><a href="http://www.egamescasino.ph/e-games/">e-Games</a></li>
-<li id="menu-item-68" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-68"><a href="http://www.egamescasino.ph/entertainment-city/">Entertainment City</a></li>
-<li id="menu-item-31" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-31"><a href="http://www.egamescasino.ph/events-and-promotions/">Events &#038; Promotions</a></li>
-<li id="menu-item-32" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-32"><a href="http://www.egamescasino.ph/featured-games/">Featured Games</a></li>
-<li id="menu-item-33" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-33"><a href="http://www.egamescasino.ph/membership/">Membership</a></li>
-<li id="menu-item-358" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-358"><a href="http://www.egamescasino.ph/house-rules/">House Rules</a></li>
-<li id="menu-item-354" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-354"><a href="http://www.egamescasino.ph/terms-and-conditions/">Terms and Conditions</a></li>
-<li id="menu-item-29" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-29"><a href="http://www.egamescasino.ph/contact-us/">Contact Us</a></li>
-</ul></div>                        
+                        <div class="menu-footer-menu-container">
+                            <ul id="footer-navigation" class="menu"><li id="menu-item-28" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-28"><a href="http://www.egamescasino.ph/">Home</a></li>
+                                <li id="menu-item-30" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-30"><a href="http://www.egamescasino.ph/e-games/">e-Games</a></li>
+                                <li id="menu-item-68" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-68"><a href="http://www.egamescasino.ph/entertainment-city/">Entertainment City</a></li>
+                                <li id="menu-item-31" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-31"><a href="http://www.egamescasino.ph/events-and-promotions/">Events &#038; Promotions</a></li>
+                                <li id="menu-item-32" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-32"><a href="http://www.egamescasino.ph/featured-games/">Featured Games</a></li>
+                                <li id="menu-item-33" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-33"><a href="http://www.egamescasino.ph/membership/">Membership</a></li>
+                                <li id="menu-item-358" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-358"><a href="http://www.egamescasino.ph/house-rules/">House Rules</a></li>
+                                <li id="menu-item-354" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-354"><a href="http://www.egamescasino.ph/terms-and-conditions/">Terms and Conditions</a></li>
+                                <li id="menu-item-29" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-29"><a href="http://www.egamescasino.ph/contact-us/">Contact Us</a></li>
+                            </ul>
+                        </div>                        
                      </div>
                 </div>
 
                 <div class="row-fluid">
                     <div class="span3 offset9">
-                        <img src="http://www.egamescasino.ph/wp-content/uploads/2013/06/membership_card.png" alt="VIP Rewards Card" title="VIP Rewards Card">
-                        <img src="http://www.egamescasino.ph/wp-content/themes/pegs_theme_v3_1/img/nounder21.png" alt="No Under 21" title="No Under 21">
+                        <img src="/wp-content/themes/pegs_themev5/img/membership_card.png" alt="VIP Rewards Card" title="VIP Rewards Card">
+                        <img src="/wp-content/themes/pegs_themev5/img/nounder21.png" alt="No Under 21" title="No Under 21">
                     </div>
                 </div>
-
             </div>
 
         </div><!-- #page-container -->
-
-            </body>
-
+</body>
 </html>
--- Membership/templates/headertemplate.php
+++ Membership/templates/headertemplate.php
@@ -3,30 +3,53 @@
 
     <head>
 
-        <title>PEGS Website Registration</title>
+        <title>e-Games Website</title>
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link href="http://www.egamescasino.ph/wp-content/themes/pegs_theme_v3_1/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="/wp-content/themes/pegs_themev5/css/bootstrap.min.css" rel="stylesheet" media="screen">
 
-        <link href="http://www.egamescasino.ph/wp-content/themes/pegs_theme_v3_1/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+        <link href="/wp-content/themes/pegs_themev5/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
 
-        <link href="http://www.egamescasino.ph/wp-content/themes/pegs_theme_v3_1/css/style.css" rel="stylesheet" media="screen">
+        <link href="/wp-content/themes/pegs_themev5/css/style.css" rel="stylesheet" media="screen">
 
         <meta name='robots' content='noindex,nofollow' />
-<script type='text/javascript' src='http://www.egamescasino.ph/wp-includes/js/jquery/jquery.js?ver=1.8.3'></script>
-<script type='text/javascript' src='http://www.egamescasino.ph/wp-content/plugins/pegs-egames-teaser/js/jquery.imagesloaded.js?ver=3.5.1'></script>
-<script type='text/javascript' src='http://www.egamescasino.ph/wp-content/plugins/pegs-egames-teaser/js/dk-image-rotator-widget.js?ver=3.5.1'></script>
-<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://www.egamescasino.ph/xmlrpc.php?rsd" />
-<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://www.egamescasino.ph/wp-includes/wlwmanifest.xml" /> 
-<meta name="generator" content="WordPress 3.5.1" />
+        <script type='text/javascript' src='/wp-includes/js/jquery/jquery.js?ver=1.8.3'></script>
+        <script type='text/javascript' src='/wp-content/plugins/pegs-egames-teaser/js/jquery.imagesloaded.js?ver=3.5.1'></script>
+        <script type='text/javascript' src='/wp-content/plugins/pegs-egames-teaser/js/dk-image-rotator-widget.js?ver=3.5.1'></script>
+        <script type='text/javascript' src='/wp-content/plugins/google-analyticator/external-tracking.min.js?ver=6.4.4.3'></script>
+        <link rel="EditURI" type="application/rsd+xml" title="RSD" href="/xmlrpc.php?rsd" />
+        <link rel="wlwmanifest" type="application/wlwmanifest+xml" href="/wp-includes/wlwmanifest.xml" /> 
+        <meta name="generator" content="WordPress 3.5.1" />
+        <meta name="title" value="Page not found  |  e-Games Website" />
+        <meta itemprop="name" value="Page not found  | e-Games Website" />
+        <meta property="og:title" value="Page not found  | e-Games Website" />
 
 	<style type='text/css'>
 		.breadcrumb {
                     font-size: 12px;
 		}
 	</style>
-	        
+	<!-- Google Analytics Tracking by Google Analyticator 6.4.4.3: http://www.videousermanuals.com/google-analyticator/ -->
+        <!-- e-Games-->
+        <script type="text/javascript">
+                var analyticsFileTypes = [''];
+                var analyticsEventTracking = 'enabled';
+        </script>
+        <script type="text/javascript">
+                var _gaq = _gaq || [];
+                _gaq.push(['_setAccount', 'UA-7240274-36']);
+                _gaq.push(['_addDevId', 'i9k95']); // Google Analyticator App ID with Google 
+
+                _gaq.push(['_trackPageview']);
+
+                (function() {
+                        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+                })();
+        </script>
+        
     </head>
 
     <body>
@@ -35,47 +58,50 @@
 
             <div class="row-fluid" id="header-container">
 
-               <div class="span4"><a href="http://www.egamescasino.ph"><img src="http://www.egamescasino.ph/wp-content/uploads/2013/05/logo.jpg"></a></div>
+               <div class="span4"><a href="http://www.egamescasino.ph"><img src="/wp-content/uploads/2013/05/logo.jpg"></a></div>
                
-               <div class="span4 pull-right" style="text-align:right;" id="search-container"><form method="get" id="searchform" action="http://www.egamescasino.ph/">
-    
-    <input type="text" name="s" id="search-box-upper"  placeholder="Search...">
-    
-    <button type="submit" name="submit" id="search-button-upper"><img src="http://www.egamescasino.ph/wp-content/themes/pegs_theme_v3_1/img/magnify.png"></button>
-    
-</form>               
-                                                       <div id="social-buttons-container">
+               <div class="span4 pull-right" style="text-align:right;" id="search-container">
+               
+                    <form method="get" id="searchform" action="http://www.egamescasino.ph/">
 
-                            <div class="row-fluid">
+                         <input type="text" name="s" id="search-box-upper"  placeholder="Search...">
 
-                                <div class="pull-right">
-                                    <a href="http://www.facebook.com/egamescasino"><img src="http://www.egamescasino.ph/wp-content/themes/pegs_theme_v3_1/img/fb.png" alt="Facebook" title="Facebook"></a>                                    
-                                    <a href="http://www.twitter.com/egamescasino"><img src="http://www.egamescasino.ph/wp-content/themes/pegs_theme_v3_1/img/twitter.jpg" alt="Twitter" title="Twitter"></a>
-                                    <a href=""><img src="http://www.egamescasino.ph/wp-content/themes/pegs_theme_v3_1/img/foursquare_icon.png" alt="Foursquare" title="Foursquare"></a>
-                                    <a href=""><img src="http://www.egamescasino.ph/wp-content/themes/pegs_theme_v3_1/img/instagram_icon.png" alt="Instagram" title="Instagram"></a>
-                                </div>
+                         <button type="submit" name="submit" id="search-button-upper"><img src="/wp-content/themes/pegs_themev5/img/magnify.png"></button>
 
+                    </form>               
+                    <div id="social-buttons-container">
+
+                        <div class="row-fluid">
+
+                            <div class="pull-right span12">
+                             <a href="http://www.facebook.com/egamescasino"><img src="/wp-content/themes/pegs_themev5/img/fb.png" alt="Facebook" title="Facebook"></a>                                    
+                             <a href="http://www.twitter.com/egamescasino"><img src="/wp-content/themes/pegs_themev5/img/twitter.png" alt="Twitter" title="Twitter"></a>
+                             <a href=""><img src="/wp-content/themes/pegs_themev5/img/foursquare_icon.png" alt="Foursquare" title="Foursquare"></a>
+                             <a href=""><img src="/wp-content/themes/pegs_themev5/img/instagram_icon.png" alt="Instagram" title="Instagram"></a>
                             </div>
 
-                        </div><!-- #social-buttons-container --></div>
+                        </div>
+
+                    </div><!-- #social-buttons-container -->
+              </div>
 
            </div><!-- #header-container-->
 
            <div id="nav-container">
-               
                <div class="menu-main-navigation-container"><ul id="main-navigation" class="menu"><li id="menu-item-23" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-23"><a href="http://www.egamescasino.ph/e-games/">e-Games</a></li>
-<li id="menu-item-26" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-26"><a href="http://www.egamescasino.ph/membership/">Membership</a></li>
-<li id="menu-item-70" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-70"><a href="http://www.egamescasino.ph/entertainment-city/">Entertainment City</a></li>
-<li id="menu-item-25" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-25"><a href="http://www.egamescasino.ph/featured-games/">Featured Games</a></li>
-<li id="menu-item-24" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-24"><a href="http://www.egamescasino.ph/events-and-promotions/">Events &#038; Promotions</a></li>
-<li id="menu-item-363" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-363"><a href="http://www.egamescasino.ph/responsible-gaming/">Responsible Gaming</a></li>
-<li id="menu-item-207" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-207"><a href="http://www.egamescasino.ph/play-for-free/">Play for Free</a></li>
-</ul></div>
+                    <li id="menu-item-26" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-26"><a href="https://membership.egamescasino.ph/login.php">Membership</a></li>
+                    <li id="menu-item-70" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-70"><a href="http://www.egamescasino.ph/entertainment-city/">Entertainment City</a></li>
+                    <li id="menu-item-25" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-25"><a href="http://www.egamescasino.ph/featured-games/">Featured Games</a></li>
+                    <li id="menu-item-24" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-24"><a href="http://www.egamescasino.ph/events-and-promotions/">Events &#038; Promotions</a></li>
+                    <li id="menu-item-363" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-363"><a href="http://www.egamescasino.ph/responsible-gaming/">Responsible Gaming</a></li>
+                    <li id="menu-item-207" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-207"><a href="http://www.egamescasino.ph/play-for-free/">Play for Free</a></li>
+                    </ul>
+               </div>
            </div><!-- #nav-container -->
 
             <div id="header-image-container">
 
-                <img src="http://www.egamescasino.ph/wp-content/themes/pegs_theme_v3_1/img/banner.jpg">                    
+                <img src="/wp-content/uploads/2013/05/membership1.jpg">                    
             </div>
 
             <div id="main-content">
