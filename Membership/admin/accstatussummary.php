9f842b3f04fd2dc349e3a40bbede40e4b645160d
Edson L. Perez <elperez@philweb.com.ph>
2013-08-05 14:40:19 +0800

    account status summary report fixes
    Membership_Release Notes_REF19_080513

--- Membership/admin/accstatussummary.php
+++ Membership/admin/accstatussummary.php
@@ -25,6 +25,11 @@ $countActive = count($memInfoActive);
 $countBanned = count($memInfoBanned);
 $countStatus = count($memInfoStatus);
 
+//Clear the session for Redemtion
+if(isset($_SESSION['CardRed'])){
+    unset($_SESSION['CardRed']);
+}
+
 //Get the total number of Both Active and Banned Account Status.
 if ($countStatus > 0) {
     $ctr = 0;
@@ -34,38 +39,47 @@ if ($countStatus > 0) {
     } while ($ctr != $countStatus);
 }
 
-//Get the total number of Active Account Status only.
+//check if active accounts count are greater than zero
 if ($countActive > 0) {
     $ctr1 = 0;
     do {
         $memInfoActive[0]['StatusValue'] = $memInfoActive[0]['Status'] == 1 ? "Active" : ($memInfoActive[0]['Status'] == 5 ? "Banned" : "");
-        $memInfoActive[0]['PercentValue'] = round(($memInfoActive[0]['COUNT(MID)'] / $memInfoStatusCount) * 100);
+        $memInfoActive[0]['PercentValue'] = number_format(($memInfoActive[0]['COUNT(MID)'] / $memInfoStatusCount) * 100, 2, '.', '');
         $memInfoActive[0]['PercentValue'] = $memInfoActive[0]['PercentValue'] . " %";
         $ctr1 = $ctr1 + 1;
     } while ($ctr1 != $countActive);
+} else {
+        $memInfoActive[0]['StatusValue'] = "Active";
+        $memInfoActive[0]['PercentValue'] = number_format(0 * 100, 2, '.', '');
+        $memInfoActive[0]['PercentValue'] = $memInfoActive[0]['PercentValue'] . " %";
+}
 
-    foreach ($memInfoActive as $value) {
-        $newrow[] = $value;
-    }
+foreach ($memInfoActive as $value) {
+    $newrow[] = $value;
 }
 
 //Put the result of Active Account Status to Grid.
 
 $result1 = $newrow;
 $datagrid_bn = new DataGrid();
-$datagrid_bn->AddColumn("Status", "'StatusValue", DataGridColumnType::Text, DataGridColumnAlignment::Center);
+$datagrid_bn->AddColumn("Status", "StatusValue", DataGridColumnType::Text, DataGridColumnAlignment::Center);
 $datagrid_bn->AddColumn("Count", "COUNT(MID)", DataGridColumnType::Text, DataGridColumnAlignment::Center);
 $datagrid_bn->AddColumn("Percentage", "PercentValue", DataGridColumnType::Text, DataGridColumnAlignment::Center);
 $datagrid_bn->DataItems = $result1;
 
+//check if banned accounts count are greater than zero
 if ($countBanned > 0) {
     $ctr1 = 0;
     do {
         $memInfoBanned[0]['StatusValue'] = $memInfoBanned[0]['Status'] == 1 ? "Active" : ($memInfoBanned[0]['Status'] == 5 ? "Banned" : "");
-        $memInfoBanned[0]['PercentValue'] = round(($memInfoBanned[0]['COUNT(MID)'] / $memInfoStatusCount) * 100);
+        $memInfoBanned[0]['PercentValue'] = number_format(($memInfoBanned[0]['COUNT(MID)'] / $memInfoStatusCount) * 100, 2, '.', '');
         $memInfoBanned[0]['PercentValue'] = $memInfoBanned[0]['PercentValue'] . " %";
         $ctr1 = $ctr1 + 1;
     } while ($ctr1 != $countBanned);
+} else {
+        $memInfoBanned[0]['StatusValue'] = "Banned";
+        $memInfoBanned[0]['PercentValue'] = number_format(0 * 100, 2, '.', '');
+        $memInfoBanned[0]['PercentValue'] = $memInfoBanned[0]['PercentValue'] . " %";
 }
 foreach ($memInfoBanned as $value) {
     $newrow[] = $value;
