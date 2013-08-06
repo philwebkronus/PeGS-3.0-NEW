cdb182b58178a91f69a90502e7cffcae8653ce34
April Rose Depliyan <aqdepliyan@philweb.com.ph>
2013-08-06 19:29:57 +0800

    Membership Updates

--- Membership/admin/template/couponredemptiontemplate.php
+++ Membership/admin/template/couponredemptiontemplate.php
@@ -93,40 +93,38 @@
             </td>
         </tr>
         <tr>
-            <td style="vertical-align:top;" colspan ="2" style="font-size: 9pt;">
+            <td style="vertical-align:top;" colspan ="2" align="justify" style=" font-size: 9pt;">
                 <br/>
                 <hr/>
                 <strong>Raffle Mechanics</strong> <br/>
-                1. Raffle Promo is open to all e-Games VIP Rewards Card holders.<br/>
-                2. Every 50 points earned in the VIP Rewards card entitles its holder to one (1) raffle coupon.<br/>
-                3. A player who wishes to participate in the raffle must:<br/>
-                <ol type="a">
-                    <li>Present VIP Rewards Card to the cashier<br/></li>
-                    <li>Update Profile (Mobile Number and Email Address required)<br/></li>
-                </ol>
-                    
-                4. A copy of the electronic raffle coupon (e-coupon) will be sent to the player’s registered email address. Transaction reference code will also be sent to the registered mobile number.<br/>
-                5. Raffle draw will be done at PhilWeb Corporate Head Office, 19th Floor Alphaland Southgate Tower 2258 Roces Avenue corner Edsa, Makati City under the supervision of a PAGCOR Representative and PhilWeb Audit Team.<br/>
-                6. Winning numbers will be announced during the raffle draw which will be videotaped and streamed live. Winners will also be informed through their registered contact details.<br/><br/>
+                1. Every 50 points earned in the Membership Card or VIP Rewards card entitles its holder to one (1) raffle e-coupon.<br/>
+                2. There will be no physical raffle coupons. All players with Membership Card may choose to redeem via the cashier or through the online Membership Portal found in the e-Games website. The cashier and the online notification that will be sent to the registered email reiterate that winners will have to provide one (1) valid ID to claim their prize. Player information on the e-coupon should match with that of the identification card presented otherwise, he/she will be disqualified.<br/>
+                3. A copy of the accomplished e-coupon, containing player information and redemption details, will be sent to the player’s registered email address. Transaction reference code will also be sent to the registered mobile number.<br/>
+                4. Raffle draw will be done at PhilWeb Corporate Head Office, 19th Floor Alphaland Southgate Tower 2258 Roces Avenue corner Edsa, Makati City under the supervision of a PAGCOR Representative and PhilWeb Audit Team.<br/>
+                5. Winning e-Coupons will be announced during the raffle draw which will be videotaped and streamed live at http://www.ustream.tv/channel/egamescasino-membershipraffle-2013. This link is still subject to change. Winners will also be informed through their registered contact details.<br/><br/>
                 <strong>Claiming of Prizes  </strong><br/>
                 1. Winners must bring the following when claiming their prize:
-                <ol type="a">
+                 <ol style="list-style-type:lower-alpha; list-style-position:inside; text-indent: 5px;">
                     <li>Printed copy of the winning e-Coupon or Redemption receipt SMS from e-Games. Winners will be asked to provide the 13-digit alphanumeric serial number indicated in the winning e-Coupon or SMS<br/></li>
-                    <li>VIP Rewards Card<br/></li>
-                    <li>One (1) government-issued identification card (ex. Passport, Driver's license, Voter's ID, SSS ID, GSIS ID, TIN ID, PAG-IBIG ID, etc.) Winner’s registered details should match with that of the identification card presented otherwise he/she will be disqualified.<br/></li>
+                    <li>Membership Card or VIP Rewards Card<br/></li>
+                    <li>One (1) valid government-issued identification card (ex. Passport, Driver's license, Voter's ID, SSS ID, GSIS ID, TIN ID, PAG-IBIG ID, etc.). Winner’s registered details should match with that of the identification card presented otherwise he/she will be disqualified.<br/></li>
                 </ol>
                 2. Prizes must be claimed by the actual winners. Authorization letters will not be honored.<br/>
                 3. Major and Minor prizes can be redeemed at PhilWeb Corporate Head Office, 19th Floor Alphaland Southgate Tower 2258 Chino Roces Avenue corner EDSA, Makati City. Consolation Prizes can be claimed at the e-Games Cafe indicated in the winning raffle coupon.<br/>
-                4. Prizes are convertible to slots load.<br/>
-                5. Prizes can be claimed at least three (3) weeks from the draw date, depending on the availability of the item.<br/>
-                6. If any prize is not claimed within 60 days after the draw date, a replacement winner will be drawn in a draw date and time to be determined by PhilWeb from the remaining qualifying participants in the draw, in accordance with the same process and procedures as applicable to the original draw.<br/><br/>
+                4. Prizes can be claimed at least three (3) weeks from the draw date, depending on the availability of the item. If any prize is not claimed within eight (8) weeks after the draw date, a replacement winner will be drawn in a draw date and time to be determined by PhilWeb from the remaining qualifying participants in the draw, in accordance with the same process and procedures as applicable to the original draw.<br/><br/>
                 <strong>Terms and Conditions</strong><br/>
-                1. The raffle promo entitles the player to win only once (i.e. if a player wins more than once, the prize with the higher monetary value will be given).<br/>
+                1. The raffle promo entitles the player to win only once.<br/>
                 2. PhilWeb reserves the right to invalidate an entry or prize winner if the proper data or authorization was not provided. If an entrant does not truthfully provide all requested personal information, PhilWeb may solely determine that such entrant shall not be eligible to take part in any way in the raffle promo or win any prize.<br/>
-                3. If the raffle draw is postponed to another date for any reason whatsoever, the new draw date will be within one (1) week from the original draw date.<br/>
+                3. Prizes with monetary value of PhP10,000 and above are subject to 20% withholding tax, which shall be for the account of the winners. As withholding agent, PhilWeb will process, withhold, and remit to BIR the 20% withholding tax in behalf of the winners. Upon receipt of tax payment, PhilWeb will issue winners an official receipt and a copy of BIR Form 2306 Certificate at Final Tax Withheld at Source. <br/>
                 4. Prizes are non-transferable and non-convertible to cash. <br/>
-                5. Prizes are subject to withholding tax, which shall be for the account of the winners. The winners shall furnish PhilWeb a copy of the BIR Return evidencing correct payment of the said withholding tax.<br/>
-                6. The decision of e-Games Management shall be final in the event of any dispute.<br/>
+                5. Prizes, other than the Toyota 86 grand prize, are convertible to slots load. The Toyota 86 has already been pre-ordered due to the limited supply of this car model in the country. Monetary value of the prize (less withholding tax) may be converted to casino bet vouchers. Electronic vouchers will be issued by e-Games Marketing and PhilWeb Top-up. Vouchers are good as cash and can be redeemed at the specified e-Games café/s within 30 days from date of issuance. Vouchers are transferable but not convertible to cash. <br/>
+                <ol style="list-style-type:lower-alpha; list-style-position:inside; text-indent: 5px;">
+                    <li>Winner may use up to PhP30, 000 bet voucher credits per day.<br/></li>
+                    <li>Winner must play for at least one (1) hour to redeem his winnings.<br/></li>
+                    <li>Winner cannot collect winnings from the cashier if bet voucher credits were used to play any non-slot games.<br/></li>
+                    <li>Cash reloads cannot be done when voucher is used as initial deposit.<br/></li>
+                    <li>Voucher cannot be used to reload current game.<br/></li>
+                </ol>
                 <br/>                   
 
             </td>
