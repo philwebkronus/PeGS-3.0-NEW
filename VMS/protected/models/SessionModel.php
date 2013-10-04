8671b9223d7179359cdc0244ce66e2cf64cb847a
Edson L. Perez <elperez@philweb.com.ph>
2013-10-04 16:50:22 +0800

    recommit Session model

--- VMS/protected/models/SessionModel.php
+++ VMS/protected/models/SessionModel.php
@@ -7,19 +7,19 @@ class SessionModel extends CFormModel{
 
     public function addSession($aid, $session_id){
         $query = "INSERT INTO accountsessions (AID, SessionID, DateCreated) VALUES ($aid, '$session_id', NOW())";
-        $sql = Yii::app()->db->createCommand($query);
+        $sql = Yii::app()->db2->createCommand($query);
         return $sql->execute();
     }
     
     public function updateSession($aid, $session_id){
         $query = ("UPDATE accountsessions SET SessionID = '$session_id', DateCreated = NOW() WHERE AID = $aid");
-        $sql = Yii::app()->db->createCommand($query);
+        $sql = Yii::app()->db2->createCommand($query);
         return $sql->execute();
     }
     
     public function checkSession($aid){
         $query = ("SELECT AID FROM accountsessions where AID = $aid");
-        $sql = Yii::app()->db->createCommand($query);
+        $sql = Yii::app()->db2->createCommand($query);
         $result = $sql->queryAll();
 
         foreach ($result as $row5) {
@@ -30,7 +30,7 @@ class SessionModel extends CFormModel{
     
     public function getAID($user){
         $query = ("SELECT AID FROM accounts where UserName = '$user'");
-        $sql = Yii::app()->db->createCommand($query);
+        $sql = Yii::app()->db2->createCommand($query);
         $result = $sql->queryAll();
 
         foreach ($result as $row5) {
@@ -42,13 +42,13 @@ class SessionModel extends CFormModel{
 
     public function deleteSession($aid){
         $query = ("DELETE FROM accountsessions where AID = $aid");
-        $sql = Yii::app()->db->createCommand($query);
+        $sql = Yii::app()->db2->createCommand($query);
         $sql->execute();
     }
     
     public function deleteSessionwithId($sessionid){
         $query = ("DELETE FROM accountsessions where SessionID = '$sessionid'");
-        $sql = Yii::app()->db->createCommand($query);
+        $sql = Yii::app()->db2->createCommand($query);
         $sql->execute();
     }
     
@@ -56,7 +56,7 @@ class SessionModel extends CFormModel{
     public function hasSession($aid){
         
         $query = ("SELECT AID, SessionID FROM accountsessions where AID = $aid");
-        $sql = Yii::app()->db->createCommand($query);
+        $sql = Yii::app()->db2->createCommand($query);
         $result = $sql->queryAll();
 
         foreach ($result as $row5) {
@@ -69,7 +69,7 @@ class SessionModel extends CFormModel{
     
     public function checkifsessionexist($aid, $sessionid){
         $query = "SELECT COUNT(AID) FROM accountsessions WHERE AID = $aid AND SessionID = '$sessionid'";
-        $sql = Yii::app()->db->createCommand($query);
+        $sql = Yii::app()->db2->createCommand($query);
         $result = $sql->queryAll();
         
         foreach ($result as $row) {
