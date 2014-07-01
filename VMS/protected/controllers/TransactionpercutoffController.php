0ae46d431f5fb18a2a2a4dc39dd05542a0587931
web-mccalderon <mccalderon@philweb.com.ph>
2014-07-01 14:23:21 +0800

    39th commit 07012014
    Fix on the error of the controller of Transaction Per Cut-off for
    Coupons module

--- VMS/protected/controllers/TransactionpercutoffController.php
+++ VMS/protected/controllers/TransactionpercutoffController.php
@@ -433,5 +433,345 @@ class TransactionpercutoffController extends VMSBaseIdentity
         
         return array("SumCount" => $sumCount, "SumValue" => $sumValue);
     }
+    
+    public function actionCoupon()
+    {
+        $_AccountSessions = new SessionModel();
+        $_TransPerCutOff = new TransactionpercutoffForm();
+        $_Coupons = new CouponModel();
+        $_Tickets = new TicketModel();
+        $_Terminals = new TerminalsModel();
+
+        if (isset(Yii::app()->session['SessionID'])) {
+            $aid = Yii::app()->session['AID'];
+            $sessionid = Yii::app()->session['SessionID'];
+        } else {
+            $sessionid = 0;
+            $aid = 0;
+        }
+
+        $sessioncount = $_AccountSessions->checkifsessionexist($aid, $sessionid);
+
+        if ($sessioncount == 0) {
+            Yii::app()->user->logout();
+            $this->redirect(array(Yii::app()->defaultController));
+        } else {
+            $display = 'none';
+            Yii::app()->session['display'] = $display;
+            $rawData = array();
+            Yii::app()->session['rawData'] = $rawData;
+            $display = 'block';
+            if (isset($_POST['TransactionpercutoffForm'])) {
+                $_TransPerCutOff->attributes = $_POST['TransactionpercutoffForm'];
+                $data = $_TransPerCutOff->attributes;
+                Yii::app()->session['transactiondate'] = $data['transactiondate'] . " " . Yii::app()->params->cutofftimestart;
+                Yii::app()->session['site'] = $data['site'];
+                Yii::app()->session['vouchertype'] = $data['vouchertype'];
+                Yii::app()->session['status'] = $data['status'];
+
+                $transdate = Yii::app()->session['transactiondate'];
+                $site = Yii::app()->session['site'];
+                $vouchertype = Yii::app()->session['vouchertype'];
+                $status = Yii::app()->session['status'];
+                $rawData = array();
+                if ($site == 'All' && $vouchertype == 'All') {
+                    $couponarr = $_Coupons->getAllUsedCouponList($transdate);
+                    $ticketarr = $_Tickets->getAllUsedTicketList($transdate);
+                } elseif ($site != 'All' && $vouchertype == 'All') {
+                    $couponarr = $_Coupons->getUsedCouponListBySite($site, $transdate);
+                    $ticketarr = $_Tickets->getUsedTicketListBySite($site, $transdate);
+                } elseif ($site == 'All' && $vouchertype == 1) {
+                    $ticketarr = $_Tickets->getAllUsedTicketList($transdate);
+                } elseif ($site == 'All' && $vouchertype == 2) {
+                    $couponarr = $_Coupons->getAllUsedCouponList($transdate);
+                } elseif ($site != 'All' && $vouchertype == 1) {
+                    $ticketarr = $_Tickets->getUsedTicketListBySite($site, $transdate);
+                } elseif ($site != 'All' && $vouchertype == 2) {
+                    $couponarr = $_Coupons->getUsedCouponListBySite($site, $transdate);
+                }
+
+                if (!empty($couponarr)) {
+                    foreach ($couponarr as $value) {
+                        $sitename = $value['SiteName'];
+                        $couponvouchertypeid = $value['VoucherTypeID'];
+                        $couponvouchercode = $value['VoucherCode'];
+                        $couponstatus = $value['Status'];
+                        $couponterminalid = $value['TerminalID'];
+                        $terminalname = $_Terminals->getTerminalNamesUsingTerminalID($couponterminalid);
+                        $couponterminalname = $terminalname[0]['TerminalName'];
+                        $couponamount = $value['Amount'];
+                        $coupondatecreated = $value['DateCreated'];
+                        $coupondateexpiry = $value['ValidToDate'];
+                        $coupondateupdated = $value['DateUpdated'];
+                        $couponiscreditable = $value['IsCreditable'];
+                        $couponsource = "Cashier";
+                        if ($couponstatus == 3) {
+                            $couponstatus = 'Used';
+                        } else {
+                            $couponstatus = '';
+                        }
+
+                        $vcoupondateupdated = new DateTime($coupondateupdated);
+                        $zcoupondateupdated = date(date_format($vcoupondateupdated, 'Y-m-d H:i:s'));
+                        $vcoupondateexpiry = new DateTime($coupondateexpiry);
+                        $zcoupondateexpiry = date(date_format($vcoupondateexpiry, 'Y-m-d H:i:s'));
+
+                        $record = array(
+                            'SiteName' => $sitename,
+                            'TerminalName' => $couponterminalname,
+                            'VoucherType' => $couponvouchertypeid,
+                            'VoucherCode' => $couponvouchercode,
+                            'Status' => $couponstatus,
+                            'Amount' => $couponamount,
+                            'DateCreated' => $zcoupondateupdated,
+                            'DateExpired' => $zcoupondateexpiry,
+                            'Source' => $couponsource,
+                            'IsCreditable' => $couponiscreditable,
+                        );
+
+                        array_push($rawData, $record);
+                    }
+                }
+                if (!empty($ticketarr)) {
+                    foreach ($ticketarr as $value) {
+                        $sitename = $value['SiteName'];
+                        $ticketvouchertypeid = $value['VoucherTypeID'];
+                        $ticketvouchercode = $value['VoucherCode'];
+                        $ticketstatus = $value['Status'];
+                        $ticketterminalid = $value['TerminalID'];
+                        $terminalname = $_Terminals->getTerminalNamesUsingTerminalID($ticketterminalid);
+                        $ticketterminalname = $terminalname[0]['TerminalName'];
+                        $ticketamount = $value['Amount'];
+                        $ticketdatecreated = $value['DateCreated'];
+                        $ticketdateexpiry = $value['ValidToDate'];
+                        $ticketsource = 'EGM';
+                        $ticketiscreditable = $value['IsCreditable'];
+
+                        $vticketdatecreated = new DateTime($ticketdatecreated);
+                        $zticketdatecreated = date(date_format($vticketdatecreated, 'Y-m-d H:i:s'));
+                        $vticketdateexpiry = new DateTime($ticketdateexpiry);
+                        $zticketdateexpiry = date(date_format($vticketdateexpiry, 'Y-m-d H:i:s'));
+                        if ($ticketstatus == 3) {
+                            $ticketstatus = 'Used';
+                        } else {
+                            $ticketstatus = '';
+                        }
+                        $record = array(
+                            'SiteName' => $sitename,
+                            'TerminalName' => $ticketterminalname,
+                            'VoucherType' => $ticketvouchertypeid,
+                            'VoucherCode' => $ticketvouchercode,
+                            'Status' => $ticketstatus,
+                            'Amount' => $ticketamount,
+                            'DateCreated' => $zticketdatecreated,
+                            'DateExpired' => $zticketdateexpiry,
+                            'Source' => $ticketsource,
+                            'IsCreditable' => $ticketiscreditable,
+                        );
+                        array_push($rawData, $record);
+                    }
+                }
+            } else {
+                $transdate = Yii::app()->session['transactiondate'];
+                $site = Yii::app()->session['site'];
+                $vouchertype = Yii::app()->session['vouchertype'];
+                $status = Yii::app()->session['status'];
+                $rawData = array();
+                if ($site == 'All' && $vouchertype == 'All') {
+                    $couponarr = $_Coupons->getAllUsedCouponList($transdate);
+                    $ticketarr = $_Tickets->getAllUsedTicketList($transdate);
+                } elseif ($site != 'All' && $vouchertype == 'All') {
+                    $couponarr = $_Coupons->getUsedCouponListBySite($site, $transdate);
+                    $ticketarr = $_Tickets->getUsedTicketListBySite($site, $transdate);
+                } elseif ($site == 'All' && $vouchertype == 1) {
+                    $ticketarr = $_Tickets->getAllUsedTicketList($transdate);
+                } elseif ($site == 'All' && $vouchertype == 2) {
+                    $couponarr = $_Coupons->getAllUsedCouponList($transdate);
+                } elseif ($site != 'All' && $vouchertype == 1) {
+                    $ticketarr = $_Tickets->getUsedTicketListBySite($site, $transdate);
+                } elseif ($site != 'All' && $vouchertype == 2) {
+                    $couponarr = $_Coupons->getUsedCouponListBySite($site, $transdate);
+                }
+
+                if (!empty($couponarr)) {
+                    foreach ($couponarr as $value) {
+                        $sitename = $value['SiteName'];
+                        $couponvouchertypeid = $value['VoucherTypeID'];
+                        $couponvouchercode = $value['VoucherCode'];
+                        $couponstatus = $value['Status'];
+                        $couponterminalid = $value['TerminalID'];
+                        $terminalname = $_Terminals->getTerminalNamesUsingTerminalID($couponterminalid);
+                        $couponterminalname = $terminalname[0]['TerminalName'];
+                        $couponamount = $value['Amount'];
+                        $coupondatecreated = $value['DateCreated'];
+                        $coupondateupdated = $value['DateUpdated'];
+                        $coupondateexpiry = $value['ValidToDate'];
+                        $couponsource = 'Cashier';
+                        $couponiscreditable = $value['IsCreditable'];
+                        if ($couponstatus == 3) {
+                            $couponstatus = 'Used';
+                        } else {
+                            $couponstatus = '';
+                        }
+
+                        $vcoupondateupdated = new DateTime($coupondateupdated);
+                        $zcoupondateupdated = date(date_format($vcoupondateupdated, 'Y-m-d H:i:s'));
+                        $vcoupondateexpiry = new DateTime($coupondateexpiry);
+                        $zcoupondateexpiry = date(date_format($vcoupondateexpiry, 'Y-m-d H:i:s'));
+
+                        $record = array(
+                            'SiteName' => $sitename,
+                            'TerminalName' => $couponterminalname,
+                            'VoucherType' => $couponvouchertypeid,
+                            'VoucherCode' => $couponvouchercode,
+                            'Status' => $couponstatus,
+                            'Amount' => $couponamount,
+                            'DateCreated' => $zcoupondateupdated, //pass date wherein coupon has used
+                            'DateExpired' => $zcoupondateexpiry,
+                            'Source' => $couponsource,
+                            'IsCreditable' => $couponiscreditable,
+                        );
+
+                        array_push($rawData, $record);
+                    }
+                }
+                if (!empty($ticketarr)) {
+                    foreach ($ticketarr as $value) {
+                        $sitename = $value['SiteName'];
+                        $ticketvouchertypeid = $value['VoucherTypeID'];
+                        $ticketvouchercode = $value['VoucherCode'];
+                        $ticketstatus = $value['Status'];
+                        $ticketterminalid = $value['TerminalID'];
+                        $terminalname = $_Terminals->getTerminalNamesUsingTerminalID($ticketterminalid);
+                        $ticketterminalname = $terminalname[0]['TerminalName'];
+                        $ticketamount = $value['Amount'];
+                        $ticketdatecreated = $value['DateCreated'];
+                        $ticketdateexpiry = $value['ValidToDate'];
+                        $ticketsource = 'EGM';
+                        $ticketiscreditable = $value['IsCreditable'];
+                        if ($ticketstatus == 3) {
+                            $ticketstatus = 'Used';
+                        } else {
+                            $ticketstatus = '';
+                        }
+
+                        $vticketdatecreated = new DateTime($ticketdatecreated);
+                        $zticketdatecreated = date(date_format($vticketdatecreated, 'Y-m-d H:i:s'));
+                        $vticketdateexpiry = new DateTime($ticketdateexpiry);
+                        $zticketdateexpiry = date(date_format($vticketdateexpiry, 'Y-m-d H:i:s'));
+
+                        $record = array(
+                            'SiteName' => $sitename,
+                            'TerminalName' => $ticketterminalname,
+                            'VoucherType' => $ticketvouchertypeid,
+                            'VoucherCode' => $ticketvouchercode,
+                            'Status' => $ticketstatus,
+                            'Amount' => $ticketamount,
+                            'DateCreated' => $zticketdatecreated,
+                            'DateExpired' => $zticketdateexpiry,
+                            'Source' => $ticketsource,
+                            'IsCreditable' => $ticketiscreditable,
+                        );
+
+                        array_push($rawData, $record);
+                    }
+                }
+            }
+            Yii::app()->session['rawData'] = $rawData;
+            $display = 'block';
+            Yii::app()->session['display'] = $display;
+            $this->render('coupon', array('model' => $_TransPerCutOff));
+        }
+    }
+    
+    public function actionExportToExcelCoupon() {
+        $_AccountSessions = new SessionModel();
+        $model = new TransactionpercutoffForm();
+        if (isset(Yii::app()->session['SessionID'])) {
+            $aid = Yii::app()->session['AID'];
+            $sessionid = Yii::app()->session['SessionID'];
+        } else {
+            $sessionid = 0;
+            $aid = 0;
+        }
+
+        $sessioncount = $_AccountSessions->checkifsessionexist($aid, $sessionid);
+
+        if ($sessioncount == 0) {
+            Yii::app()->user->logout();
+            $this->redirect(array(Yii::app()->defaultController));
+        } else {
+            $data = Yii::app()->session['rawData'];
+            //code to download the data of report in the excel format
+            include_once("protected/extensions/ExportToExcel.php");
+            $transdate = Yii::app()->session['transactiondate'];
+            $transdate = explode(' ', $transdate);
+            //code to download the data of report in the excel format
+            $fn = "TransactionPerCutOff_" . $transdate[0] . ".xls";
+            //create the instance of the exportexcel format
+            $excel_obj = new ExportExcel("$fn");
+            //setting the values of the headers and data of the excel file
+            //and these values comes from the other file which file shows the data
+            
+            $table = "<table>";
+            $table .= "<tr><td></td></tr><tr><td>Code</td><td>Site</td><td width='100px;'>Terminal</td><td width='100px;'>Amount</td><td width='180px;'>Transaction Date</td><td width='180px;'>Date Expired</td><td width='100px;'>Source</td><td width='100px;'>Is Creditable</td><td width='100px;'>Status</td></tr>";
+
+            foreach ($data as $vview) {
+                if ($vview['IsCreditable'] == 1) {
+                    $creditable = "Creditable";
+                } else if ($vview['IsCreditable'] == 2) {
+                    $creditable = "Not Creditable";
+                } else {
+                    $creditable = "";
+                }
+
+                $table .= "<tr><td>" . $vview['VoucherCode'] . "</td><td>" . $vview['SiteName'] . "</td><td>" . $vview['TerminalName'] . "</td><td align='right'>'" . number_format($vview['Amount'], 2, ".", ",") . "</td><td>" . $vview['DateCreated'] . "</td><td>" . $vview['DateExpired'] . "</td><td>" . $vview['Source'] . "</td><td>" . $creditable . "</td><td>" . $vview['Status'] . "</td></tr>";
+            }
+            $table .= "</table>";
+            $excel_obj->toHTML($table);
+        }
+    }
+    
+    public function actionTransactionPerCutOffDataTable($rawData) {
+        $_AccountSessions = new SessionModel();
+
+        if (isset(Yii::app()->session['SessionID'])) {
+            $aid = Yii::app()->session['AID'];
+            $sessionid = Yii::app()->session['SessionID'];
+        } else {
+            $sessionid = 0;
+            $aid = 0;
+        }
+
+        $sessioncount = $_AccountSessions->checkifsessionexist($aid, $sessionid);
+        if ($sessioncount == 0) {
+            Yii::app()->user->logout();
+            $this->redirect(array(Yii::app()->defaultController));
+        } else {
+            $arrayDataProvider = new CArrayDataProvider($rawData, array(
+                'keyField' => false,
+                'pagination' => array(
+                    'pageSize' => 10,
+                ),
+            ));
+            $params = array(
+                'arrayDataProvider' => $arrayDataProvider,
+            );
+
+            if (!isset($_GET['ajax'])) {
+                if($this->action->id == 'ticket') {
+                    $this->renderPartial('transactionpercutofftickets', $params);
+                } else {
+                    $this->renderPartial('transactionpercutoff', $params);
+                }
+            } else {
+                 if($this->action->id == 'ticket') {
+                    $this->renderPartial('transactionpercutofftickets', $params);
+                } else {
+                    $this->renderPartial('transactionpercutoff', $params);
+                }
+            }
+        }
+    }
 }
 ?>
