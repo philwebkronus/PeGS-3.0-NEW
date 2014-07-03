dd35c9d10d3a38dba5e71a90d5f681a812578e58
web-mgesguerra <mgesguerra@philweb.com.ph>
2014-07-03 15:23:08 +0800

    Voucher API Updates
    Fix invoker overview

--- VoucherAPI/protected/views/wsVapiInvoker/overview.php
+++ VoucherAPI/protected/views/wsVapiInvoker/overview.php
@@ -6,14 +6,14 @@
     <?php //echo CHtml::link('Use', 'use', array('target'=>'_blank')); ?>
     <br />-->
     <br />
-    <?php echo CHtml::link('VerifyTicket', 'verifyTicket', array('target'=>'_blank')); ?>
+    <?php echo CHtml::link('VerifyTicket', Yii::app()->createUrl('wsVapiInvoker/verifyTicket'), array('target'=>'_blank')); ?>
     <br />
     <br />
-    <?php echo CHtml::link('AddTicket', 'addTicket', array('target'=>'_blank')); ?>
+    <?php echo CHtml::link('AddTicket', Yii::app()->createUrl('wsVapiInvoker/addTicket'), array('target'=>'_blank')); ?>
     To be consumed in KAPI: RedeemSession and StAPI: CancelDeposit
     <br />
     <br />
-    <?php echo CHtml::link('UseTicket', 'useTicket', array('target'=>'_blank')); ?>
+    <?php echo CHtml::link('UseTicket', Yii::app()->createUrl('wsVapiInvoker/useTicket'), array('target'=>'_blank')); ?>
     To be consumed only in StAPI: LogStackerTransaction
     <br />
     <br />
