b622b8d5143969f7fe18c49ef6307a3e933b49d5
JR Jagolino <gvjagolino@philweb.com.ph>
2014-07-08 16:34:33 +0800

    Update Kronus Cashier abbott genesis
    update query in sitegrossholdcutoff model

--- Kronus/Cashier.Abbott.Genesis/app/frontend/models/SiteGrossHoldCuttOffModel.php
+++ Kronus/Cashier.Abbott.Genesis/app/frontend/models/SiteGrossHoldCuttOffModel.php
@@ -5,7 +5,7 @@ class SiteGrossHoldCuttOffModel extends MI_Model{
     
     public function getrunningActiveTickets($date, $enddate, $siteid) {
         
-        $sql = 'SELECT SUM(RunningActiveTickets) AS RunningActiveTicketsTotal FROM npos.sitegrossholdcutoff WHERE SiteID = :site_id AND DateFirstTransaction >= :startdate AND DateFirstTransaction < :enddate';
+        $sql = 'SELECT SUM(RunningActiveTickets) AS RunningActiveTicketsTotal FROM npos.sitegrossholdcutoff WHERE SiteID = :site_id AND DateCutOff >= :startdate AND DateCutOff < :enddate';
         $param = array(':site_id'=>$siteid, ':startdate'=>$date, ':enddate'=>$enddate);
         $this->exec($sql, $param);
         $result = $this->find();
