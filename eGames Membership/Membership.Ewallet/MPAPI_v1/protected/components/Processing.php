474b1e4a365238963807fcf9625b45311d6be876
web-mccalderon <mccalderon@philweb.com.ph>
2015-06-17 11:49:48 +0800

    Revert "Update for MPAPI v1 e-SAFE version"
    This reverts commit 5d5855a89682c1c814811c8737e2442e597d9ef5.

--- eGames Membership/Membership.Ewallet/MPAPI_v1/protected/components/Processing.php
+++ eGames Membership/Membership.Ewallet/MPAPI_v1/protected/components/Processing.php
@@ -22,6 +22,7 @@ class Processing
         $couponRedemptionLogsModel = new CouponRedemptionLogsModel();
         $logger = new ErrorLogger();
         $apiLogsModel = new APILogsModel();
+        $pcwsWrapper = new PcwsWrapper();
         
         $apiMethod = 8;
         $oldCurrentPoints = 0;
@@ -44,18 +45,23 @@ class Processing
                     
                     if(count($availableCoupon) == $quantity) {
                         //get current points for validation
-                        $playerPoints = $memberCardsModel->getMemberPointsAndStatus($cardNumber);
-  
+                        //$playerPoints = $memberCardsModel->getMemberPointsAndStatus($cardNumber);
+                        $playerPoints = $pcwsWrapper->getCompPoints($cardNumber, 0);
+                        $playerPoints = $playerPoints['GetCompPoints']['CompBalance']; 
                         
-                        $oldCurrentPoints = $playerPoints['CurrentPoints'];
+                        $oldCurrentPoints = $playerPoints;
                         if($oldCurrentPoints >= $redeemedPoints) {
                             //update card points (deduct the total redeemed points)
                             $isPointsUpdated = $memberCardsModel->updateCardPoints($MID, $redeemedPoints);
-                            
-                            $playerPoints = $memberCardsModel->getMemberPointsAndStatus($cardNumber);
+                            $amt = $oldCurrentPoints - $redeemedPoints;
+                            $isPointsDeducted = $pcwsWrapper->deductCompPoints($cardNumber, $amt, '', 0);
+                            $playerPoints = $pcwsWrapper->getCompPoints($cardNumber, 0);
+                            $playerPoints = $playerPoints['GetCompPoints']['CompBalance'];
+                            $oldCurrentPoints = $playerPoints;
+                            //$playerPoints = $memberCardsModel->getMemberPointsAndStatus($cardNumber);
                             
                             //check if points is greater than or equal to 0
-                            if($isPointsUpdated > 0 && $playerPoints['CurrentPoints'] >= 0) {
+                            if($isPointsUpdated > 0 && $isPointsDeducted && $oldCurrentPoints >= 0) {
                                 
                                 //get and check status of the reward item
                                 $isActive = $rewardItemsModel->checkStatus($rewardItemID);
@@ -333,6 +339,7 @@ class Processing
         $itemRedemptionLogsModel = new ItemRedemptionLogsModel();
         $apiLogsModel = new APILogsModel();
         $helpers = new Helpers();
+        $pcwsWrapper = new PcwsWrapper();
         
         $AID = $MID;
         $totalPoints = $redeemedPoints/$quantity;
@@ -349,12 +356,19 @@ class Processing
                     //check item serial code availability
                     $availableSerialCode = $itemSerialCodesModel->getAvailableSerialCodeCount($rewardItemID, 1);
                     if(count($availableSerialCode) == 1) {
-                        $playerPoints = $memberCardsModel->getMemberPointsAndStatus($cardNumber);
-                        $oldCurrentPoints = $playerPoints['CurrentPoints'];
-                        if($playerPoints['CurrentPoints'] >= $totalPoints) {
+                        //$playerPoints = $memberCardsModel->getMemberPointsAndStatus($cardNumber);
+                        $playerPoints = $pcwsWrapper->getCompPoints($cardNumber, 0);
+                        $playerPoints = $playerPoints['GetCompPoints']['CompBalance'];
+                        $oldCurrentPoints = $playerPoints;
+                        if($oldCurrentPoints >= $totalPoints) {
                             $isPointsUpdated = $memberCardsModel->updateCardPoints($MID, $totalPoints);
-                            $playerPoints = $memberCardsModel->getMemberPointsAndStatus($cardNumber);
-                            if($isPointsUpdated > 0 && $playerPoints['CurrentPoints'] > 0) {
+                            $amt = $oldCurrentPoints - $totalPoints;
+                            $isPointsDeducted = $pcwsWrapper->deductCompPoints($cardNumber, $amt, '', 0);
+                            //$playerPoints = $memberCardsModel->getMemberPointsAndStatus($cardNumber);
+                            $playerPoints = $pcwsWrapper->getCompPoints($cardNumber, 0);
+                            $playerPoints = $playerPoints['GetCompPoints']['CompBalance'];
+                            $oldCurrentPoints = $playerPoints;
+                            if($isPointsUpdated > 0 && $oldCurrentPoints > 0 && $isPointsDeducted) {
                                 $currentItemCount = $rewardItemsModel->getAvailableItemCount($rewardItemID);
                                 if($currentItemCount['AvailableItemCount'] >= $itemQtyItr && $currentItemCount['AvailableItemCount'] != 0) {
                                     $isItemCountUpdated = $rewardItemsModel->updateAvailableItemCount($rewardItemID, $AID);
--- eGames Membership/Membership.Ewallet/MPAPI_v1/protected/models/Ref_SMSApiMethodsModel.php
+++ eGames Membership/Membership.Ewallet/MPAPI_v1/protected/models/Ref_SMSApiMethodsModel.php
@@ -46,3 +46,4 @@ class Ref_SMSApiMethodsModel {
     
             
 }
+
