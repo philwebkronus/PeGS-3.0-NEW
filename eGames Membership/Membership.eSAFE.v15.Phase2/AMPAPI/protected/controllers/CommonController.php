789fb857338c6ad1dd35a1b18cf71ede2b64c716
web-mccalderon <mccalderon@philweb.com.ph>
2015-10-22 16:34:10 +0800

    Minor update for CommonController.php 10222015
    Minor update for CommonController.php 10222015

--- eGames Membership/Membership.eSAFE.v15.Phase2/AMPAPI/protected/controllers/CommonController.php
+++ eGames Membership/Membership.eSAFE.v15.Phase2/AMPAPI/protected/controllers/CommonController.php
@@ -7,20 +7,20 @@
  */
 
 class CommonController {
-    
+
     public static function retMsg($module, $transMsg = '',$errorCode='', $username = '', $password = '', $TPSessionID='', $MID='', $sessionID='', $cardTypeID='',$isVip='', $cardNumber='', $MPSessionID='', $CurrentPoints='', $RewardID='') { //$errorCode = '') {//,$idPresented = '', $nationality = '', $occupation = '', $isSmoker = '', $MID = '') {
 
         if($module == 'Login') {
             return array('Login' => array('MPSessionID'=>$MPSessionID,'CardTypeID'=>$cardTypeID,'IsVIP' => $isVip,'CardNumber' => $cardNumber, 'ErrorCode'=>$errorCode, 'ReturnMessage' => $transMsg));
         }
         else if($module=='AuthenticateSession'){
-            return array('AuthenticateSession' => array( 
+            return array('AuthenticateSession' => array(
                 'TPSessionID'=>$TPSessionID,
                 'ErrorCode' => $errorCode,
                 'ReturnMessage' => $transMsg,
                     ));
         }
-        
+
         else if($module=='GetActiveSession'){
             return array('GetActiveSession' => array(
                 'ErrorCode' => $errorCode,
@@ -29,52 +29,52 @@ class CommonController {
             ));
         }
         else if($module == 'ChangePassword') {
-            return array('ChangePassword' => array('ErrorCode' => $errorCode, 
+            return array('ChangePassword' => array('ErrorCode' => $errorCode,
                          'ReturnMessage' => $transMsg));
         }
         else if($module == 'ForgotPassword') {
-            return array('ForgotPassword' => array('ErrorCode' => $errorCode, 
+            return array('ForgotPassword' => array('ErrorCode' => $errorCode,
                          'ReturnMessage' => $transMsg));
         }
         else if($module == 'UpdateProfile') {
             return array('UpdateProfile' => array('ErrorCode' => $errorCode,
                          'ReturnMessage' => $transMsg));
         }
-        
+
         else if($module == 'GetGender') {
              return array('GetGender' => array('ErrorCode'=> $errorCode,
                 'ReturnMessage' => $transMsg));
 //            return array('GetGender' => array('Gender'=>array(array(
 //                'GenderID' => $errorCode,
 //                'GenderDescription' => $transMsg))));
-            
+
         }
-        
+
         else if($module == 'GetIDPresented') {
             return array($module => array('ErrorCode'=> $errorCode,
                 'ReturnMessage' => $transMsg));
         }
-        
+
         else if($module == 'GetNationality') {
             return array($module => array('ErrorCode'=> $errorCode,
                 'ReturnMessage' => $transMsg));
         }
-        
+
         else if($module == 'GetOccupation') {
             return array($module => array('ErrorCode'=> $errorCode,
                 'ReturnMessage' => $transMsg));
         }
-        
+
         else if($module == 'GetIsSmoker') {
             return array($module => array('ErrorCode'=> $errorCode,
                 'ReturnMessage' => $transMsg));
         }
-        
+
         else if($module == 'CheckPoints') {
             return array('CheckPoints' => array('CurrentPoints'=>$CurrentPoints,'CardNumber'=>'','ErrorCode' => $errorCode,
                          'ReturnMessage' => $transMsg));
         }
-        
+
         else if($module == 'ListItems') {
             return array('ListItems' => array(
                 'RewardID'=>'',
@@ -103,14 +103,14 @@ class CommonController {
                 'ErrorCode' => $errorCode,
                 'ReturnMessage' => $transMsg));
         }
-        
+
         else if($module == 'RegisterMember') {
             return array('RegisterMember' => array('ErrorCode' => $errorCode,
                          'ReturnMessage' => $transMsg));
         }
-        
+
         else if($module == 'RedeemItems') {
-            
+
                 return array('RedeemItems' => array(
                 'ItemImage'=>'',
                 'ItemName'=>'',
@@ -139,10 +139,10 @@ class CommonController {
                 'Terms'=>'',
                 'ErrorCode' => $errorCode,
                 'ReturnMessage' => $transMsg));
-          
+
         }
-        
-        
+
+
         else if($module == 'GetProfile') {
             return array('GetProfile' => array(
                 'FirstName'=>'',
@@ -170,20 +170,20 @@ class CommonController {
 		'RegionID'=>'',
 		'CityID'=>'',
                 'ErrorCode'=>$errorCode,
-                'ReturnMessage'=>$transMsg,    
+                'ReturnMessage'=>$transMsg,
                 ));
         }
-        
+
         else if($module == 'GetReferrer') {
             return array($module => array('ErrorCode'=> $errorCode,
                 'ReturnMessage' => $transMsg));
         }
-        
+
         else if($module == 'GetRegion') {
             return array($module => array('ErrorCode'=> $errorCode,
                 'ReturnMessage' => $transMsg));
         }
-        
+
         else if($module == 'GetCity') {
             return array($module => array('ErrorCode'=> $errorCode,
                 'ReturnMessage' => $transMsg));
@@ -191,23 +191,23 @@ class CommonController {
 
         else if($module == 'GetBalance') {
             return array('GetBalance' => array(
-		'WithdrawableBalance' => 0.00, 
+		'WithdrawableBalance' => 0.00,
 		'PlayableBalance' => 0.00,
 		'BonusBalance' => 0.00,
 		'PlaythroughBalance' => 0.00,
 		'ErrorCode' => $errorCode,
 		'ReturnMessage' => $transMsg));
         }
-        
+
         else if($module == 'Logout') {
             return array('Logout' => array('ErrorCode' => $errorCode,
                          'ReturnMessage' => $transMsg));
         }
-        
+
         else if($module == 'CreateMobileInfo') {
-            return array('CreateMobileInfo' => array('MPSessionID'=>$MPSessionID,'CardTypeID'=>$cardTypeID,'IsVIP' => $isVip,'CardNumber' => '','ErrorCode'=>$errorCode, 'ReturnMessage' => $transMsg, 'Remarks' => ''));           
+            return array('CreateMobileInfo' => array('MPSessionID'=>$MPSessionID,'CardTypeID'=>$cardTypeID,'IsVIP' => $isVip,'CardNumber' => '','ErrorCode'=>$errorCode, 'ReturnMessage' => $transMsg, 'Remarks' => ''));
         }
-        
+
         else if($module == 'ListPromos') {
             return array('ListPromos' => array(
                 'SiteName'=>'',
@@ -222,7 +222,7 @@ class CommonController {
                 'ErrorCode' => $errorCode,
                 'ReturnMessage' => $transMsg));
         }
-        
+
         else if($module == 'ListLocations') {
             return array('ListLocations' => array(
                 'SiteName' => '',
@@ -231,6 +231,7 @@ class CommonController {
                 'OperatingHours'=>'',
                 'Latitude'=> 0.000000,
                 'Longitude'=> 0.000000,
+                'CategoryID'=>'',
                 'ErrorCode' => $errorCode,
                 'ReturnMessage' => $transMsg));
         }
@@ -239,64 +240,64 @@ class CommonController {
     public static function retMsgLogin($module, $mpSessionID, $cardTypeID, $isVIP, $cardNumber, $errorCode, $transMsg, $alterStr) {
         if($module == 'Login')
             return array('Login' => array('MPSessionID' => $mpSessionID, 'CardTypeID' => $cardTypeID, 'IsVIP' => $isVIP, 'CardNumber' => $cardNumber, 'ErrorCode' => $errorCode, 'ReturnMessage' => $transMsg, 'Remarks' => $alterStr));
-            
+
     }
-    
+
     public static function retMsgCheckPoints($module, $currentPoints, $errorCode, $transMsg) {
         if($module == 'CheckPoints')
             return array('CheckPoints' => array('CurrentPoints' => $currentPoints, 'ErrorCode' => $errorCode, 'ReturnMessage' => $transMsg));
     }
-    
+
     public static function retMsgUpdateProfile($module, $errorCode, $transMsg) {
         $module = 'UpdateProfile';
         return array($module => array('ErrorCode' => $errorCode, 'ReturnMessage' => $transMsg));
     }
-    
+
     public static function retMsgGetGender($module, $gender) {
         $module = 'GetGender';
         return array($module => array('Gender' => $gender));
     }
-    
+
     public static function retMsgGetIDPresented($module, $idPresented) {
         $module = 'GetIDPresented';
         return array($module => array('PresentedID' => $idPresented));
     }
-    
+
     public static function retMsgGetNationality($module, $nationality) {
         $module = 'GetNationality';
         return array($module => array('Nationality' => $nationality));
     }
-    
+
     public static function retMsgGetOccupation($module, $occupation) {
         $module = 'GetOccupation';
         return array($module => array('Occupation' => $occupation));
     }
-    
+
     public static function retMsgGetIsSmoker($module, $isSmoker) {
         $module = 'GetIsSmoker';
         return array($module => array('IsSmoker' => $isSmoker));
     }
-    
+
     public static function retMsgListItems($module, $listOfItems, $errorCode, $transMsg) {
         $module = 'ListItems';
         return array($module => array('ItemsList' => $listOfItems, 'ErrorCode' => $errorCode, 'ReturnMessage' => $transMsg));
     }
-    
+
     public static function retMsgRegisterMember($module, $errorCode, $transMsg) {
         $module = 'RegisterMember';
         return array($module => array('ErrorCode' => $errorCode, 'ReturnMessage' => $transMsg));
     }
-    
+
     public static function retMsgGetProfile($module, $profile, $errorCode, $transMsg) {
         $module = 'GetProfile';
         return array($module => array('Profile' => $profile, 'ErrorCode' => $errorCode, 'ReturnMessage' => $transMsg));
     }
-    
+
     public static function retMsgChangePassword($module, $errorCode, $transMsg) {
         $module = 'ChangePassword';
         return array($module => array('ErrorCode' => $errorCode, 'ReturnMessage' => $transMsg));
     }
-    
+
     public static function retMsgGetBalance($module, $withdrawableBalance, $playableBalance, $bonusBalance, $playthroughBalance, $errorCode, $transMsg) {
         $module = 'GetBalance';
         return array($module => array('WithdrawableBalance' => $withdrawableBalance, 'PlayableBalance' => $playableBalance, 'BonusBalance' => $bonusBalance, 'PlaythroughBalance' => $playthroughBalance, 'ErrorCode' => $errorCode, 'ReturnMessage' => $transMsg));
@@ -307,10 +308,10 @@ class CommonController {
     public static function retMsgListPromos($module, $listOfPromos, $errorCode, $transMsg) {
         return array($module => array('PromosList' => $listOfPromos, 'ErrorCode' => $errorCode, 'ReturnMessage' => $transMsg));
     }
-    
+
     public static function retMsgListLocations($module, $listOfLocations, $errorCode, $transMsg) {
         return array($module => array('LocationsList' => $listOfLocations, 'ErrorCode' => $errorCode, 'ReturnMessage' => $transMsg));
     }
-    
+
 }
 ?>
