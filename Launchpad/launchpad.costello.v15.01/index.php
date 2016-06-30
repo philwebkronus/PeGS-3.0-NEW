e078d63caaac4efc512704be908d22b9ab6cdc17
web-dsrabena <dsrabena@philweb.com.ph>
2016-06-30 14:53:01 +0800

    Launchpad updates
    Launchpad updates

---
+++ Launchpad/launchpad.costello.v15.01/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html><head>
+<meta http-equiv="refresh" content="0; url=http://172.16.104.174/launchpad.v15.costello.02/views/lobby.php" />
+</head>
+<title>Screen Saver</title>
+<body>
+<a href="http://172.16.104.174/launchpad.v15.costello.02/views/lobby.php">Click here to continue.</a>
+</body>
+</html>
