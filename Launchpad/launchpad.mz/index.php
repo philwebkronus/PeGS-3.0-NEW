e98bb00be4e48af7ff7c42c569dafbcabb99a704
PhilWeb Corporation <itsysadmin@philweb.com.ph>
2019-04-01 15:11:52 +0800

    removal of copied version

--- Launchpad/launchpad.mz/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html><head>
-<meta http-equiv="refresh" content="0; url=http://192.168.41.4/launchpad/launchpad.eSAFEV15/views/lobby.php" />
-</head>
-<title>Screen Saver</title>
-<body>
-<a href="http://192.168.41.4/launchpad/launchpad.eSAFEV15/views/lobby.php">Click here to continue.</a>
-</body>
-</html>
