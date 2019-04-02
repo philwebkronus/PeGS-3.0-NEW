b0e0d4625969c7f1f886bbaf1d3172e1382392fd
web-javida <javida@philweb.com.ph>
2019-04-02 10:47:16 +0800

    Update Queries of TerminalServices.php

--- MZAPI/MZAPI.mz/protected/models/TerminalServices.php
+++ MZAPI/MZAPI.mz/protected/models/TerminalServices.php
@@ -9,32 +9,6 @@ class TerminalServices extends CFormModel
         $this->connection = Yii::app()->db;
     }
     
-      public function updateterminalpassword($password, $hashedpassword,$terminalid, $terminalidvip,$serviceid) {
-        $startTrans = $this->connection->beginTransaction();
-        try {
-            $sql = "UPDATE terminalservices SET ServicePassword = :password, HashedServicePassword = :hashed, Status = 1, isCreated = 1
-                             WHERE TerminalID IN (:terminalid,:terminalidvip) AND ServiceID = :serviceid";
-            $param = array(':password' => $password, ':hashed' => $hashedpassword, 
-                        ':terminalid'=>$terminalid, ':terminalidvip'=>$terminalidvip,':serviceid'=>$serviceid);
-            $command = $this->connection->createCommand($sql);
-            $command->bindValues($param);
-            $command->execute();
-            try {
-                $startTrans->commit();
-                return 1;
-            } catch (PDOException $e) {
-                $startTrans->rollback();
-                Utilities::log($e->getMessage());
-                return 0;
-            }
-             
-        } catch (Exception $e) {
-            $startTrans->rollback();
-            Utilities::log($e->getMessage());
-            return 0;
-        }
-    }
-    
     public function getTerminalDetails($temrinalid,$serviceid)
     {
         $sql = "SELECT * FROM terminalservices WHERE TerminalID = :terminalid AND ServiceID = :serviceid;";
